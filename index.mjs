2d8ccd5684429b7e6f32504ec79b59225339cddb
Darlan Alves <745095+darlanalves@users.noreply.github.com>
2024-07-31 11:06:42 +0200

    fix: allow multiple versions of a component

--- index.mjs
+++ index.mjs
@@ -1,27 +1,42 @@
 import createServer from "@cloud-cli/http";
-import { createReadStream, existsSync } from "fs";
-import { join, resolve } from "path";
+import { existsSync } from "fs";
+import { mkdir } from "fs/promises";
+import { join } from "path";
 
 const dataPath = process.env.DATA_PATH || join(process.cwd(), "data");
 const nameRe = /^[a-z]+-[a-z0-9]+$/;
+const versionRe = /^(\d{1,2}\.\d{1,2}\.\d{1,2}|\d{1,2}|latest)$/;
 
 createServer(async function (request, response) {
-  const url = new URL(request.url, "http://localhost");
   const { method } = request;
 
   if (method === "POST") {
     try {
       const body = Buffer.concat(await request.toArray()).toString("utf8");
       const json = JSON.parse(body);
-      const { name, source } = json;
+      const { name, source = "", version = "latest" } = json;
 
       if (!nameRe.test(name)) {
-        throw new Error("Invalid component name: " + name);
+        return badRequest(response, "Invalid component name: " + name);
       }
 
-      const file = join(dataPath, name);
+      if (!versionRe.test(version)) {
+        return badRequest(response, "Invalid component version: " + version);
+      }
+
+      if (!String(source).trim()) {
+        return badRequest(response, "Invalid component source");
+      }
+
+      const folder = join(dataPath, name);
+      const file = join(folder, version + ".mjs");
+
+      if (!existsSync(folder)) {
+        await mkdir(folder);
+      }
+
       await writeFile(file, source);
-      response.end('OK');
+      response.end("OK");
     } catch (e) {
       console.log(e);
       response.writeHead(500).end("Internal error");
@@ -30,19 +45,12 @@ createServer(async function (request, response) {
     return;
   }
 
-  if (method === "GET") {
-    const name = resolve(url.pathname);
-    const file = join(dataPath, name);
-
-    if (existsSync(file)) {
-      response.setHeader("Cache-Control", "max-age=86400");
-      createReadStream(file).pipe(response);
-      return;
-    }
-  }
-
   notFound(response);
 });
+
+function badRequest(reason) {
+  response.writeHead(400).end(reason);
+}
 
 function notFound(response) {
   response.writeHead(404).end("Not found");
